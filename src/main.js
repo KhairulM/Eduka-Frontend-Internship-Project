d3a69eb1965584e59c9e36a41a11fcfd7d736eff
M Khairul Makirin <khairul240999@gmail.com>
2020-05-08 20:18:04 +0700

    finished home layout and added json server db file

--- src/main.js
+++ src/main.js
@@ -2,7 +2,16 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { 
+  faChevronLeft, 
+  faChevronRight,
+  faArrowCircleRight} 
+  from '@fortawesome/free-solid-svg-icons'
 
+library.add(faChevronLeft, faChevronRight, faArrowCircleRight)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.config.productionTip = false;
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,126 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div id="top-bar">
+      <button class="primary-button">
+        <font-awesome-icon icon="chevron-left" style="margin-right: 10px;"></font-awesome-icon>
+        <h4>Prev</h4>
+      </button>
+      <h2>Simulasi Ujian UTBK</h2>
+      <button class="primary-button">
+        <h4>Next</h4>
+        <font-awesome-icon icon="chevron-right" style="margin-left: 10px;"></font-awesome-icon>
+      </button>
+    </div>
+    <hr>
+
+    <div id="main-content">
+      <div id="question-box" class="box">
+        <p>hello</p>
+      </div>
+      <div id="choice-box" class="box">
+
+      </div>
+      <div id="control">
+        <div id="time-left">
+          <h2>30:00</h2>
+        </div>
+        <button class="secondary-button">
+          <h4>Submit</h4>
+          <font-awesome-icon icon="arrow-circle-right" style="margin-left: 10px;"></font-awesome-icon>
+        </button>
+      </div>
+      <div id="summary-box" class="box">
+
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "Home",
-  components: {
-    HelloWorld
-  }
 };
 </script>
+
+<style scoped>
+.home {
+  margin: 0;
+  padding: 5% 10%;
+  max-width: 700px;
+  min-width: 600px;
+}
+
+hr {
+  border: 0.5px solid rgba(0, 0, 0, 0.1);
+}
+
+button {
+  border: none;
+  text-align: center;
+  padding: 10px 15px;
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-between;
+  align-items: center;
+  cursor: pointer;
+}
+
+#top-bar {
+  display:flex;
+  flex-flow: row nowrap;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+
+#main-content{
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-gap: 20px;
+  grid-auto-rows: minmax(50px, auto);
+  margin-top: 20px;
+}
+
+#question-box {
+  grid-column: 1 / 4;
+  grid-row: 1 / auto;
+}
+
+#choice-box {
+  grid-column: 1 / 4;
+  grid-row: auto;
+}
+
+#summary-box {
+  grid-column: 4 / 6;
+  grid-row: 2 / auto;
+}
+
+#control {
+  grid-column: 4 / 6;
+  grid-row: 1;
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-around;
+  align-items: center;
+}
+
+.box{
+  border: 1px solid #f09d34;
+  border-radius: 6px;
+  padding: 20px;
+}
+
+.primary-button {
+  background-color: #f09d34;
+  border-radius: 7px;
+  color: white;
+    
+}
+
+.secondary-button{
+  background-color: #7766ec;
+  border-radius: 100px;
+  color: white;
+}
+</style>
